d3cf90f5ee20ba36a43b005afd629822c7552842
Jim Nelson <jim@jimnelson.us>
2022-02-22 17:54:32 -0800

    #FirstTry

--- class13-materials/temperature-converter/js/main.js
+++ class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,14 @@
 //Write your pseduo code first! 
+/*
+    Get value in celsius
+    convert from that to fahrenheit
+    output
+*/
+
+document.querySelector('#theButton').addEventListener('click', convertMe);
+
+function convertMe () {
+    const degC = document.querySelector('#celsius').value;
+    const degF = degC * 9 / 5 + 32;
+    document.querySelector('#output').innerText = degF;
+}
