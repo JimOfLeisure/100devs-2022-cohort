edca885edc5017d2f9f849d1d1a7357034a507e3
Jim Nelson <jim@jimnelson.us>
2022-02-22 17:41:42 -0800

    lab

--- class12-materials/class-weekend-boring/js/main.js
+++ class12-materials/class-weekend-boring/js/main.js
@@ -2,9 +2,15 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase();
 
   //Conditionals go here
-
+  if (['tuesday', 'thursday'].some(e => e === day)) {
+    console.log('Class today');
+  } else if (['saturday', 'sunday'].some(e => e === day)) {
+    console.log('Weekend');
+  } else {
+    console.log('meh');
+  }
 
 }
