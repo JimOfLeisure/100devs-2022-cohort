3f7f5338282461434dff56bf80b8479bbe2100fd
Jim Nelson <jim@jimnelson.us>
2022-02-22 18:36:10 -0800

    bachelor 3

--- class13-materials/bachelor-code/3/js/main.js
+++ class13-materials/bachelor-code/3/js/main.js
@@ -1,11 +1,17 @@
-const contestants = document.querySelectorAll(/*INSERTCODE*/)
+const contestants = document.querySelectorAll('h2')
 
-Array.from(contestants).forEach(element => element.addEventListener('click', /*INSERTCODE*/))
+Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose))
 
 function checkForRose(click){
-	if(click.target.classList.contains(/*INSERTCODE*/)){
-		document.querySelector('#nikki').classList.toggle(/*INSERTCODE*/)
+	if(click.target.classList.contains('rose')){
+		document.querySelector('#nikki').classList.toggle('hidden')
 	}else{
 		alert("Wrong!");
 	}
 }
+
+/*
+	nikki has rose
+	click on nikki, get photo
+	click on claire wrong
+*/
