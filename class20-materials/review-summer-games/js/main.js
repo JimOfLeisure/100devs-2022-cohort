f51e0d9d19256f2b954d4c6c34f40a55bb813e9a
Jim Nelson <jim@jimnelson.us>
2022-03-17 17:41:39 -0700

    summer games lab

--- class20-materials/review-summer-games/js/main.js
+++ class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,10 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+
+function multiplyArrayNums(nums) {
+    let product = 1;
+    nums.forEach(e => product =     product * e);
+    alert(product);
+}
+
+multiplyArrayNums([2,2]);
+multiplyArrayNums([3,3,3]);
