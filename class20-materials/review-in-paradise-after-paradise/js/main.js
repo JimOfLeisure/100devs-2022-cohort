b0b28b4f7474638581163b4fdfa70453a3fee42b
Jim Nelson <jim@jimnelson.us>
2022-03-17 17:10:18 -0700

    lab2

--- class20-materials/review-in-paradise-after-paradise/js/main.js
+++ class20-materials/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,15 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+function respondToArray(arr) {
+    if (arr[0] < arr.at(-1)) {
+        alert("Hi");
+    } else if (arr[0] > arr[arr.length - 1]) {
+        alert("Bye");
+    } else {
+        alert("We close in an hour");
+    }
+}
+
+respondToArray([1,2,2,2,22,50]);
+respondToArray([100,2,22,50]);
+respondToArray([50,2,2,33,3,3,2,22,50]);
