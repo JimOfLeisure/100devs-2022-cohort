7d4feb83e7a9080de1735be724432102025dd1e9
Jim Nelson <jim@jimnelson.us>
2022-03-20 22:48:16 -0700

    merge looped

--- merge-repo.sh
+++ merge-repo.sh
@@ -1,10 +1,15 @@
-dir=class-03-materials
 
+for num in $(seq 4 8); do
+dir=class0${num}-materials
+echo $dir
 pushd ../$dir
-git filter-repo --force --to-subdirectory-filter $dir
+git status
+# git filter-repo --force --to-subdirectory-filter $dir
 popd
-read foo
-git remote add $dir ../$dir \
-&& git fetch $dir --tags \
-&& git merge --allow-unrelated-histories $dir/main
-git remote remove $dir
+
+# git remote add $dir ../$dir \
+# && git fetch $dir --tags \
+# && git merge --allow-unrelated-histories $dir/main
+# git remote remove $dir
+
+done
