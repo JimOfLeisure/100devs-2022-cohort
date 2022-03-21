f64eb6321bc0ce4ddf2ce2bbdd88fbfb31e07b3a
Jim Nelson <jim@jimnelson.us>
2022-03-20 22:35:10 -0700

    merge tweak

--- merge-repo.sh
+++ merge-repo.sh
@@ -1,10 +1,10 @@
-dir=class02-materials
+dir=class-03-materials
 
 pushd ../$dir
 git filter-repo --force --to-subdirectory-filter $dir
 popd
 read foo
-git remote add classmat $dir \
-&& git fetch classmat --tags \
-&& git merge --allow-unrelated-histories classmat/main
-git remote remove classmat
+git remote add $dir ../$dir \
+&& git fetch $dir --tags \
+&& git merge --allow-unrelated-histories $dir/main
+git remote remove $dir
