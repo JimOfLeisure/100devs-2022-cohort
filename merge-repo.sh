1c6e81aad247e2ec70eb18c9acda22f84df7f5ba
Jim Nelson <jim@jimnelson.us>
2022-03-20 23:04:07 -0700

    merge formatting and tweak

--- merge-repo.sh
+++ merge-repo.sh
@@ -1,15 +1,19 @@
 
-for num in $(seq 4 8); do
-dir=class0${num}-materials
-echo $dir
-pushd ../$dir
-git status
-# git filter-repo --force --to-subdirectory-filter $dir
-popd
+for num in $(seq 0 0); do
+    dir=class2${num}-materials
+    echo $dir
 
-# git remote add $dir ../$dir \
-# && git fetch $dir --tags \
-# && git merge --allow-unrelated-histories $dir/main
-# git remote remove $dir
+    pushd ../$dir
+
+    git status
+    ### CAUTION this destructively rewrites git history of target repo
+    # git filter-repo --force --to-subdirectory-filter $dir
+
+    popd
+
+    # git remote add $dir ../$dir \
+    # && git fetch $dir --tags \
+    # && git merge -m "merge $dir" --allow-unrelated-histories $dir/main
+    # git remote remove $dir
 
 done
