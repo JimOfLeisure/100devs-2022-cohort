2284675c426d0ab90b4f3dc2bb04c856ea2b152d
Jim Nelson <jim@jimnelson.us>
2022-03-20 22:22:36 -0700

    merge script update

--- merge-repo.sh
+++ merge-repo.sh
@@ -1,5 +1,9 @@
 dir=../class02-materials
 
+pushd $dir
+git filter-repo --to-subdirectory-filter $dir
+popd
+read foo
 git remote add classmat $dir \
 && git fetch classmat --tags \
 && git merge --allow-unrelated-histories classmat/main
