f61687bbc38cb9b74414697d3cd5858697a00f9b
Jim Nelson <jim@jimnelson.us>
2022-03-22 17:13:37 -0700

    joined late, catching up with currently reviewing lab

--- class21-materials/mighty-morphin-review/js/main.js
+++ class21-materials/mighty-morphin-review/js/main.js
@@ -1,18 +1,33 @@
 // *Variables*
 // Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
 
+let bestDay = 'Talk Like a Pirate Day';
+console.log(bestDay);
+
 //Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
+
+let seeMyChars = 'This is a string thingy';
+alert(seeMyChars.slice(-3));
 
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
 
+function fiveNumberMath(a, b, c, d, e) {
+  alert(Math.abs(100 - (a + b + c + d + e)));
+}
+fiveNumberMath(-20, -20, -20, -20, -20);
 
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
 
+function threeNumberMath(a, b, c) {
+  const arr = [a, b, c];
+  console.log(Math.min(...arr));
+  console.log(Math.max(...arr));
+}
+threeNumberMath(5, 6, 7);
 
 // *Conditionals*
 //Create a function that returns heads or tails randomly and as fairly as possible. Call the function.
-
 
 //*Loops*
 //Create a function that takes in a number. Console log the result of heads or tails using the previous function x times where x is the number passed into the function. Call the function.
