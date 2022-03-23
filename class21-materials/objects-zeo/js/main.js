663feffe29920cbf3d692dd9f902825d477f7e41
Jim Nelson <jim@jimnelson.us>
2022-03-22 18:25:29 -0700

    my stopwatch try

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,12 @@
 //Create a stopwatch object that has four properties and three methods
+
+const stopWatch = {};
+stopWatch.color = 'black';
+stopWatch.splitTime = 1.25;
+stopWatch.split2 = 2.5;
+stopWatch.currentTime = 3.0;
+stopWatch.start = function () {};
+stopWatch.split = function (stopwatch) {
+  stopwatch.splitTime = stopwatch.currentTime;
+};
+stopWatch.stop = function () {};
