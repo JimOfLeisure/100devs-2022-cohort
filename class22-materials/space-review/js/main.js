37cd9dde83e0435479eb8cfa3e513310fea6be5b
Jim Nelson <jim@jimnelson.us>
2022-03-24 17:11:31 -0700

    completed all the space lab

--- class22-materials/space-review/js/main.js
+++ class22-materials/space-review/js/main.js
@@ -9,14 +9,30 @@ function sumAndAlert(nums) {
   );
 }
 
-sumAndAlert([1, 2, 3]);
-sumAndAlert([32, 245, 1, 6, 64, 647, 76, 67, 2, 23, 123, 3, 5, 5, 6, 6, 7, 2]);
+// sumAndAlert([1, 2, 3]);
+// sumAndAlert([32, 245, 1, 6, 64, 647, 76, 67, 2, 23, 123, 3, 5, 5, 6, 6, 7, 2]);
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
 
+function squareNums(nums) {
+  return nums.map(n => n * n);
+}
+
+console.log(squareNums([2, 5, 9]));
+
 //Create a function that takes string
 //Print the reverse of that string to the console
 
+const reverseString = s => s.split('').reverse().join('');
+
+console.log(reverseString('Hello'));
+
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+
+const isPalindrome = s =>
+  alert(['Not a palindrome', 'Palindrome!'][Number(s === reverseString(s))]);
+
+isPalindrome('leon noel');
+isPalindrome('foo');
