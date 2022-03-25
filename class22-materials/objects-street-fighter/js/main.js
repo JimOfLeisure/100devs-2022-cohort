a1b5246444475b86bb7d40a797eebdfed8d1f498
Jim Nelson <jim@jimnelson.us>
2022-03-24 18:29:50 -0700

    fight character constructor

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,22 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighter(name, height, speed, strength) {
+  this.name = name || 'Mysterious Hero';
+  this.height = height || 6;
+  this.speed = speed | 5;
+  this.strength = strength || 5;
+  this.punch = () => {
+    console.log('Hiyah!');
+  };
+  this.kick = () => {
+    console.log('Oof');
+  };
+  this.dodge = () => {
+    console.log('Missed me!');
+  };
+}
+
+const link = new StreetFighter('Link', 4);
+const pikachu = new StreetFighter('Pikachu', 3, 8, 4);
+
+console.log(link, pikachu);
