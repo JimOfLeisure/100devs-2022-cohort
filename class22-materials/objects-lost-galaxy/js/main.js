64c8a51bef7d06367493cc39267520576ebf96cb
Jim Nelson <jim@jimnelson.us>
2022-03-24 17:50:57 -0700

    mouse object

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,10 @@
 //Create a mouse object that has four properties and three methods
+
+const mouse = {
+  furColor: 'gray',
+  feetCount: 4,
+  tailCount: 1,
+  skitter: () => 'EEEEeeeeek!',
+  jump: () => 'Even worse',
+  eat: () => "Hey that's my food",
+};
