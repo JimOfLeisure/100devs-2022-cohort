518287678b7e331296fc3469ff95edf0c9eeda30
Jim Nelson <jim@jimnelson.us>
2022-03-24 17:52:59 -0700

    oh, four

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -4,6 +4,7 @@ const mouse = {
   furColor: 'gray',
   feetCount: 4,
   tailCount: 1,
+  weight: 500,
   skitter: () => console.log('EEEEeeeeek!'),
   jump: () => console.log('Even worse'),
   eat: () => console.log("Hey that's my food"),
