ae01aa5fbb9518c7153215a94dc42d218d37893f
Jim Nelson <jim@jimnelson.us>
2022-03-29 18:14:48 -0700

    skater / constructor

--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,15 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+function MakeSkater(sponsor, rank, wins, name, bestTrick) {
+  this.sponsor = sponsor || 'Nike';
+  this.rank = rank || 'novice';
+  this.wins = wins || 0;
+  this.name = name || 'skater';
+  this.bestTrick = bestTrick || 'Ollie';
+  this.skate = () => console.log(`${this.name} skates for ${this.sponsor}`);
+  this.trick = () => console.log(`${this.name} does the ${this.bestTrick}`);
+  this.extreme = () => console.log(`${this.name} goes EXTREME`);
+}
+
+const pat = new MakeSkater();
+const sam = new MakeSkater('Reebok', 'pro', 5, 'Sam', 'really high jump');
