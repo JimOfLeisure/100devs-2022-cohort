87469a6eaddb93aea7d81ad676d4213a059ebd19
Jim Nelson <jim@jimnelson.us>
2022-03-29 17:56:21 -0700

    objects lost galaxy

--- class23-materials/objects-lost-galaxy/js/main.js
+++ class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,11 @@
 //Create a dog object that has four properties and three methods
+
+const dog = {
+  ears: 2,
+  legs: 1,
+  eyes: 2,
+  height: 2,
+  bark: () => console.log('WOOF'),
+  whine: () => console.log('hmmmmhmmmph'),
+  bite: () => console.log('not as bad as the bark'),
+};
