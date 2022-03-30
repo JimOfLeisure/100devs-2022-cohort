b0f25f5da2e2933d579eb68293a1e3495555091e
Jim Nelson <jim@jimnelson.us>
2022-03-29 18:33:36 -0700

    did this while Leon talking bout APIs... I got the DOM manipulation before he showed before that, was looking at MDN for xmlhttprequest

--- class23-materials/objects-dog-photos/js/main.js
+++ class23-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,23 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+// I started doing this early in class; Leon is using the fetch api, not xmlhttprequest
+
+/*
+const xhr = new XMLHttpRequest();
+
+xhr.load(res => {
+  const img = document.querySelector('img');
+  console.log(res);
+});
+
+xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+*/
+
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    const img = document.querySelector('img');
+    img.src = data.message;
+  })
+  .catch(err => console.err(`error ${err}`));
