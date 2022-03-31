c26e85be230fff7771f872aa137852dfe9c90421
Jim Nelson <jim@jimnelson.us>
2022-03-31 15:47:52 -0700

    codewars from rufio stream 3/30

--- codewars/8kyu.js
+++ codewars/8kyu.js
@@ -3,3 +3,13 @@ function positiveSum(arr) {
   return arr.reduce((acc, cur) => (cur < 0 ? acc : acc + cur), 0);
 }
 // Could have done filter first, but just went with a ternary in the callback
+
+// https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
+function squareSum(numbers) {
+  return numbers.reduce((acc, cur) => cur * cur + acc, 0);
+}
+
+// https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+function countSheeps(arrayOfSheep) {
+  return arrayOfSheep.reduce((acc, cur) => (cur === true ? acc + 1 : acc), 0);
+}
