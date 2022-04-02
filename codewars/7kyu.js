bb76c8c8d525d30e0a3c9a96c0ca324cd420d184
Jim Nelson <jim@jimnelson.us>
2022-04-01 22:27:42 -0700

    codewar with verolafox's stream

--- codewars/7kyu.js
+++ codewars/7kyu.js
@@ -21,3 +21,20 @@ function descendingOrder(n) {
   - Join back to a string
   - Cast as number
 */
+
+// https://www.codewars.com/kata/5663f5305102699bad000056/train/javascript
+function mxdiflg(a1, a2) {
+  return a1.reduce((acc, x) => {
+    const maxDiff = a2.reduce((a, y) => {
+      const diff = Math.abs(y.length - x.length);
+      return diff > a ? diff : a;
+    }, -1);
+    return maxDiff > acc ? maxDiff : acc;
+  }, -1);
+}
+/*
+  Nested .reduce methods with -1 initializer
+  So empty arr will return -1
+  each a1 string (x) calculates the maxDiff of each a2 (y) length
+  Each accumulator is the largest difference
+*/
