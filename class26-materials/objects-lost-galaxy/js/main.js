61012c0c91c11000945584e9fa13304231a779c0
Jim Nelson <jim@jimnelson.us>
2022-04-07 16:51:38 -0700

    oops was supposed to change to pizza

--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
 //Create a dog object that has four properties and three methods
+
+function Dog(breed, color, name, height) {
+  this.breed = breed;
+  this.color = color;
+  this.name = name;
+  this.height = height;
+  this.bark = function () {
+    console.log('woof');
+  };
+  this.bite = function () {
+    console.log('chomp, lawsuit');
+  };
+  this.chase = function () {
+    console.log('run run run');
+  };
+}
+
+const fido = new Dog('floof', 'white', 'Fido', 3);
