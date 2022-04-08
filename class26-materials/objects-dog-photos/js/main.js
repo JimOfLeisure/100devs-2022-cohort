8bfad4dd1ace340cf5a0c0b83007535d3c153e3d
Jim Nelson <jim@jimnelson.us>
2022-04-07 18:02:01 -0700

    dog ceo

--- class26-materials/objects-dog-photos/js/main.js
+++ class26-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,12 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+const img = document.querySelector('img');
+
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then(res => res.json())
+  .then(data => {
+    // console.log(data);
+    img.src = data.message;
+    img.alt = 'image of dog';
+  })
+  .catch(err => console.err(error));
