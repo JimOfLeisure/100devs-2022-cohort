e2ae0c8966f87871e675fe9428a0bbb1ad524195
Jim Nelson <jim@jimnelson.us>
2022-04-07 18:28:44 -0700

    shots lab working checkpoint

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,1 +1,25 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+const options = {
+  method: 'GET',
+  mode: 'no-cors',
+};
+
+const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php';
+
+const search = document.querySelector('input');
+const img = document.querySelector('img');
+
+document.querySelector('button').addEventListener('click', getDrink);
+
+function getDrink(event) {
+  console.log(search.value);
+  const url = baseUrl + '?s=' + search.value;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      img.src = data.drinks[0].strDrinkThumb;
+    })
+    .catch(err => console.error(err));
+}
