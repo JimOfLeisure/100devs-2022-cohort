804eb7d1348f5b3f971941c19190d4571c83d9f5
Jim Nelson <jim@jimnelson.us>
2022-04-12 16:12:24 -0700

    class lab

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,19 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
-
+class TvShow {
+  constructor(showName, genre, rating, length) {
+    this.showName = showName;
+    this.genre = genre;
+    this.rating = rating;
+    this.length = length;
+  }
+  play() {
+    console.log(`Playing ${this.showName}`);
+  }
+  pause() {
+    console.log('pause');
+  }
+  subscribe() {
+    console.log("Oh you can't really subscribe on Netflix, can you?");
+  }
+}
