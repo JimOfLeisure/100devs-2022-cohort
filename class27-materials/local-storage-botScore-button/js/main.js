56980984afa61992d65d741cb88bcfd9308a7b6c
Jim Nelson <jim@jimnelson.us>
2022-04-12 17:44:27 -0700

    localStorage lab

--- class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,10 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+//Create a button that adds 1 to a botScore stored in localStorage
+const keyName = 'botScore';
+let botScore = Number(localStorage.getItem(keyName)) || 0;
 
+document.querySelector('button').addEventListener('click', addOne);
 
+function addOne(event) {
+  botScore += 1;
+  localStorage.setItem(keyName, botScore);
+}
