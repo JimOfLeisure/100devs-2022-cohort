36df5a41c012b3edcf8823b9b9ce62bb23b0fca9
Jim Nelson <jim@jimnelson.us>
2022-04-28 16:11:58 -0700

    partial lab; trying to figure out more properties

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,23 @@
-//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+//Create an espresso machine ~~constructor~~ class that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+    constructor(waterLevel, groundsLevel) {
+        this.waterLevel = waterLevel;
+        this.groundsLevel = groundsLevel;
+        this.makeSteam = function () {
+            console.log('gurgle gurgle gurgle');
+            this.waterLevel--;
+        };
+        this.makeEspresso = function () {
+            console.log('gurgle gurgle gurgle');
+            this.groundsLevel--;
+            this.waterLevel--;
+        };
+        this.clean = function () {
+            console.log('self-cleaning, somehow');
+        };
+    }
+}
+
+const em = new EspressoMachine(5, 5);
+em.makeEspresso();
