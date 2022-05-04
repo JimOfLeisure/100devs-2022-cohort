35759ba50034297f55d10a56ed2dfda1bfc8ac85
Jim Nelson <jim@jimnelson.us>
2022-05-03 17:12:57 -0700

    lab child class

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,32 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal {
+    constructor(name) {
+        this._name = name
+    }
+    get name() {
+        return this._name
+    }
+    speak() {
+        console.log(`${this._name} makes a sound`)
+    }
+}
+
+class Pig extends Animal {
+    constructor(name, breed) {
+        super(name);
+        this._breed = breed;
+    }
+    speak() {
+        super.speak();
+        console.log(`${this._name} says Oink!`);
+    }
+    get breed() {
+        return this._breed
+    }
+}
+
+const piglet = new Pig('Piglet', 'cartoon');
+console.log(piglet.name);
+console.log(piglet.breed);
+piglet.speak();
