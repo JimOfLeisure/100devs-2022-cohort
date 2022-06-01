dc902dac52f6996c91fc5c2fac4974f0a0611c68
Jim Nelson <jim@jimnelson.us>
2022-05-31 18:15:31 -0700

    working one api

--- class38-materials/rap-names-api/server.js
+++ class38-materials/rap-names-api/server.js
@@ -1,13 +1,22 @@
+const { response } = require('express');
 const express = require('express');
 
 const app = express();
-
 const PORT = process.env.PORT || 8000;
+
+const savage = {
+    age: 29,
+    birthName: 'Sheyaa Bin Abraham-Joseph',
+    birthLocation: 'London, England',
+}
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/api', (req, res) => {
+    res.json(savage);
+})
 app.listen(PORT, () => {
     console.log(`The server is running on port ${PORT}, and Leon is funny and awesome`);
 });
