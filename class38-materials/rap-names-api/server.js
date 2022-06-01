0505520b8759a0935b982a7386696f9070688c01
Jim Nelson <jim@jimnelson.us>
2022-05-31 18:24:59 -0700

    parameter

--- class38-materials/rap-names-api/server.js
+++ class38-materials/rap-names-api/server.js
@@ -4,18 +4,30 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-const savage = {
-    age: 29,
-    birthName: 'Sheyaa Bin Abraham-Joseph',
-    birthLocation: 'London, England',
+const rappers = {
+    '21 Savage': {
+        age: 29,
+        birthName: 'Sheyaa Bin Abraham-Joseph',
+        birthLocation: 'London, England',
+    },
+    'Chance the Rapper': {
+        age: 29,
+        birthName: 'Chancelor Bennett',
+        birthLocation: 'Chicago, Illinois',
+    },
+    'Dylan': {
+        age: 29,
+        birthName: 'Dylan',
+        birthLocation: 'Dylan',
+    },
 }
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/api', (req, res) => {
-    res.json(savage);
+app.get('/api/:rapperName', (req, res) => {
+    res.json(rappers[req.params.rapperName]);
 })
 app.listen(PORT, () => {
     console.log(`The server is running on port ${PORT}, and Leon is funny and awesome`);
