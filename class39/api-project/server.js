183b0852852a450766c78cfa742ce51f878fbfbe
Jim Nelson <jim@jimnelson.us>
2022-06-02 17:06:50 -0700

    added style & client js

--- class39/api-project/server.js
+++ class39/api-project/server.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 8000;
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+app.get('/css/style.css', (req, res) => {
+    res.sendFile(__dirname + '/css/style.css');
+});
+app.get('/js/main.js', (req, res) => {
+    res.sendFile(__dirname + '/js/main.js');
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
